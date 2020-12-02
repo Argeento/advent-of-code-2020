d2c39b91999330187c9ece6d9b336c58809cd6b0
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2020-12-02 01:04:51 +0100

    Init

---
+++ index.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const dayNr = process.argv[2] || '1'
+const partNr = process.argv[3] || '1'
+
+require('ts-node').register()
+
+console.log(`Running day-${dayNr} part-${partNr}...`)
+
+require(
+  path.resolve(
+    __dirname,
+    `day-${dayNr}`,
+    `part-${partNr}.ts`
+  )
+)
