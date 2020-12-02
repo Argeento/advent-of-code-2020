77a868328a96b23499d5f1e75190373c04b8be01
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2020-12-02 17:24:52 +0100

    Remove unnecessary interface

--- day-2/policies.ts
+++ day-2/policies.ts
@@ -2,14 +2,7 @@ import { getLinesFromFile } from '../utils'
 
 const lines = getLinesFromFile(`${__dirname}/input.txt`)
 
-interface IPolicy {
-  min: number,
-  max: number,
-  letter: string,
-  password: string
-}
-
-const policies: IPolicy[] = lines.map(line => {
+const policies = lines.map(line => {
   const match = line.match(/^(?<min>\d+)-(?<max>\d+) (?<letter>.): (?<password>.*)$/)
   const { max, min, letter, password } = match.groups
 
